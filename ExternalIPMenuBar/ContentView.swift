1edca911ebdd6127b2d17c910246004be6d464b4
zvnic <zvnic@mail.ru>
2026-04-27 08:28:08 +0300

    Initial Commit

---
+++ ExternalIPMenuBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExternalIPMenuBar
+//
+//  Created by zvnic on 27.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExternalIPMenuBar/ExternalIPMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExternalIPMenuBarApp.swift
+//  ExternalIPMenuBar
+//
+//  Created by zvnic on 27.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct ExternalIPMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
